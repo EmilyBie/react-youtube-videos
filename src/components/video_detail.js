ec3f82ffb9eb8f92da210088310a3946967e4680
Emily Bie <bieyuyu@gmail.com>
2017-04-11 20:31:31 +1000

    allow full screen of iframe

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -16,7 +16,10 @@ class VideoDetail extends Component {
     return (
       <div className="video-detail col-md-8">
         <div className="embed-responsive embed-responsive-16by9">
-          <iframe className="embed-responsive-item" src={url}></iframe>
+          <iframe 
+            className="embed-responsive-item"
+            src={url}
+            allowFullScreen="allowfullscreen"></iframe>
         </div>
         <div className="details">
           <div className="video-detail-title">
